26dfe4bfdf9714d8eb4dc36094185fb8032e7e9c
Kei Sato <kei-sato@meeetol.com>
2016-05-11 09:49:01 +0900

    echo http request

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func handler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "hi, there. %s", req.URL.Path[1:])
+	s := []string{
+		fmt.Sprintf("%s %s %s", req.Method, req.URL.Path, req.Proto),
+		fmt.Sprintf("Host: %s", req.Host),
+		fmt.Sprintf("Content-Length: %d", req.ContentLength),
+	}
+
+	for k, v := range req.Header {
+		s = append(s, fmt.Sprintf("%s: %s", k, strings.Join(v, ", ")))
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	body := buf.String()
+	if body != "" {
+		s = append(s, "\n"+body)
+	}
+
+	fmt.Fprint(rw, strings.Join(s, "\n"))
 }
 
 func main() {
